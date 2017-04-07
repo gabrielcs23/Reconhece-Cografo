f26247436dc01bd9937f6733e6e2a603822bfeb6
gabrielcs23 <gabrielcs23@hotmail.com>
2017-04-07 15:10:19 -0300

    Add files via upload
    antiga arvore de diretórios, verificar se esta tudo ok quando virar co-arvore

---
+++ arvdir.h
@@ -0,0 +1,91 @@
+#ifndef ARVDIR_H_INCLUDED
+#define ARVDIR_H_INCLUDED
+
+// FALTOU O IMPRIME
+
+//definição da estrutura de árvore de diretório
+typedef struct coarv{
+    struct coarv *pai, *filho, *irmao;
+    int marcado; // 0 marcado, 1 desmarcado, 2 marcado e desmarcado
+    int tipo; // -1 vertice, tipo 0 ou tipo 1
+    int id; // nome do vertice caso seja vertice senao -1
+}TCA;
+
+//definição dos métodos básicos de árvores
+TCA *cria (int tipo,int id){
+    TCA *a = (TCA*) malloc (sizeof(TCA));
+    a->pai = NULL;
+    a->filho = NULL;
+    a->irmao = NULL;
+    a->tipo = tipo;
+    a->id = id;
+    return a;
+};
+
+//inserção de subarvore "filho" em uma arvore "pai"
+void inserir (TCA *afilho, TCA *apai){
+    afilho->irmao = apai->filho;
+    afilho->pai = apai;
+    apai->filho = afilho;
+}
+
+//liberar arvore da memoria
+void liberar (TCA *a){
+    TCA *p = a->filho;
+    while(p){
+        TCA *t = p->irmao;
+        liberar(p);
+        p = t;
+    }
+    free(a);
+}
+/*
+//mover nó a para ser filho de nó b
+void mover(TCA *a, TCA*b){
+    if(a == a->pai->filho){
+        a->pai->filho = a->irmao;
+    } else {
+        TCA *p = a->pai->filho;
+        while(p->irmao != a) p = p->irmao;
+        p->irmao = a->irmao;
+    }
+    a->pai = b;
+    a->irmao = b->filho;
+    b->filho = a;
+    if(!a->arquivo){
+        TDir *aux = (TDir*)b->info;
+        aux->num_dir++;
+    }
+    else{
+        TDir *aux = (TDir*)b->info;
+        aux->num_arq++;
+    }
+}
+*/
+//remove um nó e seus filhos
+void destruir (TCA *a){
+    TCA *i;
+    i = a->pai->filho;
+    if(i == a) a->pai->filho = a->irmao;
+    else{
+        while (i->irmao != a) i = i->irmao;
+        i->irmao = a->irmao;
+    }
+    liberar(a);
+}
+
+//busca e retorna o nó com o nome procurando-o na subárvore a
+TCA* busca (TCA* a, char *c){
+    if(!a) return NULL;
+    if(a->id == id) return a;
+    TCA *r, *p;
+    for(p=a->filho;p;p=p->irmao){
+        r = busca(p,c);
+        if(r) return r;
+    }
+    return NULL;
+}
+
+// FALTOU O IMPRIME, ANIMAL
+
+#endif // ARVDIR_H_INCLUDED
