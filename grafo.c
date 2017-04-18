1c11af62d26307d974050786281a82460996a5df
gabrielcs23 <gabrielcs23@hotmail.com>
2017-04-18 13:19:45 -0300

    Update grafo.c

--- grafo.c
+++ grafo.c
@@ -1,29 +1,23 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf470
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+#include <stdio.h>
+#include <stdlib.h>
 
-\f0\fs24 \cf0 #include <stdio.h>\
-#include <stdlib.h>\
-\
-typedef struct viz\{\
-    int id;\
-    struct viz *prox;\
-\}TViz;\
-\
-typedef struct grafo\{\
-    int id;\
-    TViz *viz;\
-    struct grafo *prox;\
-\}TG;\
-\
-TG *inicializa(void)\{\
-    return NULL;\
-\}\
-\
-void imprime(TG *g)\{\
-    TG *p = g;\
+typedef struct vizf{
+    int id;
+    struct viz *prox;
+}TViz;
+
+typedef struct grafo{
+    int id;
+    TViz *viz;
+    struct grafo *prox;
+}TG;
+
+TG *inicializa(void){
+    return NULL;
+}
+
+void imprime(TG *g){
+    TG *p = g;
     while(p)\{\
         printf("%d\\n",p->id);\
         TViz *v = p->viz;\
